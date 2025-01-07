cc8b86a30a8430f1fea33498c68d5b174c79d86a
puspadewii <puspadft@gmail.com>
2025-01-07 20:54:22 +0800

    Update ke 2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+//ini route login
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/admin', [AdminController::class, 'admin']);
