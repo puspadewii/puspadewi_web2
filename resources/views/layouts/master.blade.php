76ab40fd4b9acec9c09d48a0166c1305603bbc16
puspadewii <puspadft@gmail.com>
2025-01-14 20:27:37 +0800

    update ke 3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 
     <title>SB Admin 2 - Dashboard</title>
+    @notifyCss
 
     <!-- Custom fonts for this template-->
     <link href="/sbadmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -220,6 +221,9 @@
     <!-- Page level custom scripts -->
     <script src="/sbadmin/js/demo/chart-area-demo.js"></script>
     <script src="/sbadmin/js/demo/chart-pie-demo.js"></script>
+
+    <x-notify::notify />
+    @notifyJs
 
 </body>
 
